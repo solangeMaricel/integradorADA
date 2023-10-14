b28b77dd5dea50c2353410525a96a936b5a35fd6
solangeMaricel <sanderson@frsf.utn.edu.ar>
2023-10-14 17:08:43 -0300

    login

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
 import express, { json } from "express";
+import { userRouter } from './routes/user-router';
 
 const app = express();
 const PORT = process.env.PORT ?? 45000;
 
 app.use(json());
 
-app.get('/ping', (req, res)=>{
-    res.json({
-        pong: 'Server runing.',
-    })
+app.get('/api', (req, res) => {
+	res.status(200).json({
+		name: 'API gestión de Restaurante',
+		version: '1.0.0',
+		running: true,
+	});
 });
+
+app.use('/api/users', userRouter);
 
 app.use('*',(req, res)=>{
     res.status(404).json({
-        
         error: 'Resource not found.',
     })
 });
-
 
 app.listen(PORT, () => {
 	console.log('Server listening on port', PORT);
